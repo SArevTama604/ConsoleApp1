42832b63e8f793ff0963c45f333952164577384d
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-03 13:13:45 -0800

    upload program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(true);
+            bool isAdult = false;
+            Console.WriteLine(isAdult);
+            if (isAdult)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are not an adult.");
+                bool hasLicense = true;
+                if (isAdult && hasLicense)
+                {
+                    Console.WriteLine("You are an adult and have a license.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Conditions not met.");
+                }
+            }
+
+
         }
     }
 }
